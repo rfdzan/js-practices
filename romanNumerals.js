73fcd3a6fe5ca664121319aa56b30c32687aa744
rfdzan <hekodev.py@gmail.com>
2024-06-04 09:41:46 +0700

    added decomposer

--- romanNumerals.js
+++ romanNumerals.js
@@ -1,3 +1,13 @@
+/**@param {number} int */
+function decomposer(int) {
+  let howManyDigits = int.toString().length;
+  let result = [];
+  for (currentDigit = 0; currentDigit < howManyDigits; currentDigit++) {
+    result.push(int.toString().at(currentDigit) + '0'.repeat(howManyDigits - currentDigit - 1));
+  }
+  return result.map((str) => parseInt(str));
+}
+
 class RomanNumerals {
   static romanNumeralsDictionary() {
     return {
@@ -18,5 +28,6 @@ class RomanNumerals {
   }
 }
 function romanNumeralMain() {
+  console.log(decomposer(15483));
 }
 romanNumeralMain();
