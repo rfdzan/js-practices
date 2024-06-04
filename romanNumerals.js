b4d4944b7aeee7d1643b959724e5c69419c699b4
rfdzan <hekodev.py@gmail.com>
2024-06-04 10:54:18 +0700

    (wip) get the left and right [k, v] which surrounds the number

--- romanNumerals.js
+++ romanNumerals.js
@@ -7,16 +7,35 @@ function decomposer(int) {
   }
   return result.map((str) => parseInt(str));
 }
+/**@param {number[]} decomposed */
+function buildRomanNumerals(decomposed) {
 
+}
+/**@param {number} int */
+function nearestLowestKey(int) {
+  let left = Object.entries(RomanNumerals.dict());
+  let right = Array.from(left);
+  right.pop()
+  left.splice(0, 1);
+  // what's left is to zip these two iterables, find where in these two does 'int' resides. then take the left [key,value].
+  let zip = left.map((leftMembers) => [])
+
+}
 class RomanNumerals {
-  static romanNumeralsDictionary() {
+  static dict() {
     return {
       "M": 1000,
+      "CM": 900,
       "D": 500,
+      "CD": 400,
       "C": 100,
+      "XC": 90,
       "L": 50,
+      "XL": 40,
       "X": 10,
+      "IX": 9,
       "V": 5,
+      "IV": 4,
       "I": 1
     };
   }
@@ -28,6 +47,7 @@ class RomanNumerals {
   }
 }
 function romanNumeralMain() {
-  console.log(decomposer(15483));
+  console.log(decomposer(3756));
+  console.log(3 % 5)
 }
-romanNumeralMain();
+nearestLowestKey(700);
